31fb57e045ef1431beb52590faf6173c5df68b20
CodeBoi420 <loghika45@gmail.com>
2025-08-08 18:35:33 -0600

    Test

---
+++ Game Jam game/Assets/Logan Folder/Scripts/UIScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool isPaused = false;
+    void Start()
+    {
+        // Limits fps to 60
+        Application.targetFrameRate = 60;
+    }
+
+    void Update()
+    {
+        if (pauseMenu)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (!isPaused) { Time.timeScale = 0; pauseMenu.SetActive(true); }
+                if (isPaused) { Time.timeScale = 1; pauseMenu.SetActive(false); }
+            }
+        }
+    }
+
+    public void MainMenu()
+    {
+        // SceneManager.LoadScene();
+        // Insert location to scene for game
+    }
+
+    public void StartGame()
+    {
+        // SceneManager.LoadScene();
+        // Insert location to scene for game
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
